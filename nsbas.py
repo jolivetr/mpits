99eae0d2637ab77957804c616e0eba63668e80c8
Romain Jolivet <jolivetinsar@gmail.com>
2018-02-16 16:26:13 +0100

    Adding the appropriate GNU license terms in the .py files

--- nsbas.py
+++ nsbas.py
@@ -1,5 +1,25 @@
 '''
 Class that implements the nsbas version of the massive Time Series.
+
+Written by R. Jolivet 2017 
+
+License:
+    MPITS: Multi-Pixel InSAR Time Series
+    Copyright (C) 2018  <Romain Jolivet>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 '''
 
 # Externals
--- petscsolver.py
+++ petscsolver.py
@@ -1,5 +1,25 @@
 '''
-Class solving the problem with the solvers in PETSc (does not really work for our case)
+Class solving the problem with the solvers in PETSc
+
+Written by R. Jolivet 2017
+
+License:
+    MPITS: Multi-Pixel InSAR Time Series
+    Copyright (C) 2018  <Romain Jolivet>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 '''
 
 import numpy as np
